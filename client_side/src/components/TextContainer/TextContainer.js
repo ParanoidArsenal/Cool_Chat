bc741999eaeac56f9b0ccdc8d7fb5d58a65f9b3e
Arsenii Baskakov <murmans1996@gmail.com>
2020-07-07 18:20:14 +0300

    made sidebar less ugly

--- client_side/src/components/TextContainer/TextContainer.js
+++ client_side/src/components/TextContainer/TextContainer.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useRef, useState, useEffect} from "react";
 import ScrollToBottom from 'react-scroll-to-bottom';
 
 import "./TextContainer.css";
@@ -14,26 +14,37 @@ const TextContainer = ({users}) => {
     //     )})}
     //     </div>
     // );
+    const [spanElement, setspanElement] = useState('❱');
+
+    const changeOpenElement = () => {
+        // return ((spanElement === '❱')?'❰' : '❱');
+        spanElement === '❱' ? setspanElement('❰') : setspanElement('❱');
+        // spanElement === '❱ Show users in chat' ? setspanElement('❰ Hide users in chat') : setspanElement('❱ Show users in chat');
+    }; 
 
     const usersContainerRef = useRef(null);
-
+    
     const showUserSidebar = (event) => {
         const container = usersContainerRef.current;
         container.classList.toggle('is-open');
+        changeOpenElement();
     };
 
     return (
         <>
         <div className = "outertextContainer" ref = {usersContainerRef}>
-        {users.map((user, i)=>{
-            return (
-            <div key = {i} className = "textContainerUser backgroundRed">
-                <p className = "textContainerText colorWhite">{user.name}</p>
+            <div  className = "usersInfo">
+                <p className = "textContainerText colorWhite">Users in chat:</p>
             </div>
-        )})}
+            {users.map((user, i)=>{
+                return (
+                <div key = {i} className = "textContainerUser">
+                    <p className = "textContainerText colorWhite">{user.name}</p>
+                </div>
+            )})}
         </div>
         <div className = "openTextContainer" onClick = {(event) => showUserSidebar(event)}>
-                <span className = "textContainerText"> ❯ </span>
+                <span className = "textContainerText"> {spanElement} </span>
         </div>
         </>
     );
